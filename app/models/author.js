00348fbdc7e006f7142a23345390df7c624e6e94
Rick Rieta <rick@vermonster.com>
2016-08-22 12:06:53 -0400

    Update author and book models

--- app/models/author.js
+++ app/models/author.js
@@ -1,6 +1,6 @@
-import DS from 'ember-data';
+import Publisher from './publisher';
+import { hasMany } from 'ember-data/relationships';
 
-export default DS.Model.extend({
-  name: DS.attr('string'),
-  books: DS.hasMany('book')
-});
+export default Publisher.extend({
+  books: hasMany('book', { async: true })
+})
