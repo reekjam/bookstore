f00dcbf1b4943f6dede8decfac88f17da33768e8
Rick Rieta <rick@vermonster.com>
2016-08-23 14:00:37 -0400

    Add modal for purchasing books

--- app/routes/books.js
+++ app/routes/books.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('book');
+  },
+
+  actions: {
+    blurBackground(blur) {
+      this.controllerFor('application').set('blur', blur)
+    }
   }
+
 });
