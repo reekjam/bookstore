1acca3c6fb17dccc3d34ea674a30a68562b0e602
Rick Rieta <rick@vermonster.com>
2016-08-24 10:47:42 -0400

    Add Show All feature

--- app/routes/books.js
+++ app/routes/books.js
@@ -1,14 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('book');
+
+  queryParams: {
+    limit: {
+      refreshModel: true
+    }
+  },
+
+  model(params) {
+    return this.store.query('book', params);
   },
 
   actions: {
+
+    showAll() {
+      const total = this.controllerFor('books').get('total');
+      console.log(total);
+      this.transitionTo({ queryParams: { limit: total }});
+    },
+
     blurBackground(blur) {
       this.controllerFor('application').set('blur', blur)
     }
+
   }
 
 });
